452efc09532f96efdcda4c34e70384136a59df6b
LucasAmerise <chuecoamerise1303@gmail.com>
2025-11-13 01:40:33 -0300

    Agregado aviso screamer

---
+++ Chubby Devs/Assets/Scripts/AvisoScreamer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AvisoScreamer : MonoBehaviour
+{
+    public AudioSource warningSound;
+    public Transform player;
+    public float distanciaMaxima = 10f;
+    private bool yaSono = false;
+
+    void Update()
+    {
+        if (!yaSono && Vector3.Distance(transform.position, player.position) < distanciaMaxima)
+        {
+            warningSound.Play();
+            yaSono = true;
+        }
+    }
+}
