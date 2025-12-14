69cd4a42aef7b14d5c44f9fe8e0e130bd42d0992
IvoTab11 <ivoagusmitasil@gmail.com>
2025-12-14 00:23:36 -0300

    Se mejoró el panel de opciones para que se cierre correctamente

--- Chubby Devs/Assets/Scripts/Menu/MenuPausa.cs
+++ Chubby Devs/Assets/Scripts/Menu/MenuPausa.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 public class MenuPausa : MonoBehaviour
 {
@@ -7,14 +8,35 @@ public class MenuPausa : MonoBehaviour
     public MonoBehaviour scriptCamara;
     public GameObject MenuSalir;
 
+    //public GameObject primerBoton;
+    private Vector3 lastMousePosition;
+
+    [Header("Panels")]
+    public GameObject panelPausa;
+    public GameObject panelOpciones;
+
+    [Header("UI")]
+    public GameObject primerBoton;
+    public GameObject primerBotonOpciones;
+
     private void Start()
     {
         Time.timeScale = 1.0f;
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        bool pausaInput =
+        Input.GetKeyDown(KeyCode.Escape) ||
+        Input.GetKeyDown(KeyCode.JoystickButton7);
+
+        if (pausaInput)
         {
+            if (panelOpciones.activeSelf)
+            {
+                panelOpciones.SetActive(false);
+                EventSystem.current.SetSelectedGameObject(primerBoton);
+                return;
+            }
             if (!Pausa)
             {
                 ObjetoMenuPausa.SetActive(true);
@@ -25,6 +47,9 @@ public class MenuPausa : MonoBehaviour
                 Cursor.lockState = CursorLockMode.None;
 
                 scriptCamara.enabled = false;
+
+                EventSystem.current.SetSelectedGameObject(primerBoton);
+                lastMousePosition = Input.mousePosition;
                /* AudioSource[] sonidos = FindObjectOfType<AudioSource>();
 
                 for (int i = 0; i< sonidos.Length; i++)
@@ -37,14 +62,38 @@ public class MenuPausa : MonoBehaviour
                 Resumir(); 
             }
         }
+
+        if (Pausa)
+        {
+            if ((Input.mousePosition - lastMousePosition).sqrMagnitude > 1f)
+            {
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+
+            lastMousePosition = Input.mousePosition;
+
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            bool joystickMoved = Mathf.Abs(h) > 0.3f || Mathf.Abs(v) > 0.3f;
+
+            if (joystickMoved && EventSystem.current.currentSelectedGameObject == null)
+            {
+                EventSystem.current.SetSelectedGameObject(primerBoton);
+            }
+        }
     }
 
     public void Resumir()
     {
         ObjetoMenuPausa.SetActive(false);
+        panelOpciones.SetActive(false);
         MenuSalir.SetActive(false);
+
         Pausa = false;
-        Time.timeScale = 1;
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
+
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         scriptCamara.enabled = true;
